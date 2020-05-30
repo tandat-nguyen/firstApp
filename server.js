ca17b2986779a0657e065109d24a32416ec65341
Nguyen Tan Dat <tandatit19@gmail.com>
2020-05-30 19:01:53 +0700

    Hello

---
+++ server.js
@@ -0,0 +1,15 @@
+
+const http = require('http');
+const express = require('express');
+const fs = require ('fs');
+
+const firstApp = express();
+
+firstApp.get('/', (req,res) =>
+  res.send('Hello world')
+)
+
+const server = http.createServer(firstApp);
+
+server.listen(1234)
+console.log("start server")
