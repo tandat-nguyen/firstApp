b6c3b4741ead0d1369f18289d9f69857b651f6d8
Nguyen Tan Dat <tandatit19@gmail.com>
2020-05-31 07:26:54 +0700

    hello

--- database.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-mongoose.connect('mongodb://root:root12345@ds041228.mlab.com:41228/heroku_64fw1q3v', {useNewUrlParser: true, useUnifiedTopology: true});
-
-const Cat = mongoose.model('Cat', { name: String });
-
-const kitty = new Cat({ name: 'Zildjian' });
-kitty.save().then(() => console.log('meow'));
